a94162087c5c3bd4d237c3163272951c405b2420
baby snaps <jmw686@nyu.edu>
2018-02-18 19:56:08 -0600

    ~~

---
+++ productivity/src/components/Todo.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+
+  <div class="todo">
+
+    <input type="text" placeholder="Add Todo" v-model="titleText">
+    <button type="button" @click="createTodo">Add</button>
+
+
+  </div>
+
+
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      titleText: '',
+      project: '',
+      times: []
+
+    };
+  },
+
+  methods: {
+    createTodo: function () {
+
+    }
+  }
+
+}
+</script>
+
+<style lang="css">
+</style>
