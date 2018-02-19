8ff616ead4fb4b44cd4c93d7d68cb4944e276ce1
baby snaps <jmw686@nyu.edu>
2018-02-18 23:44:12 -0600

    vuejs version

---
+++ productivity/src/App.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <div>
+    <todo></todo>
+  </div>
+
+</template>
+
+<script>
+
+import todo from './components/Todo.vue'
+
+
+export default {
+
+  components: {
+    'todo': todo
+  },
+  data() {
+
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+
+}
+
+
+</script>
+
+<style lang="css">
+
+
+</style>
