6c6ecd5b06cb3d4b751a92288de9d55ba22febf8
Windows Letgoner <jmw686@nyu.edu>
2018-03-17 01:27:09 -0500

    progress to complete anim

--- productivity/src/components/Todo.vue
+++ productivity/src/components/Todo.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div  class="card ui todo">
     <div>
-      <div class="history">
+      <!-- <div class="history"> -->
         <button type="button"@click="deleteTodo(todo)" class="remove">
           <i class="trash alternate icon"></i>
         </button>
@@ -26,39 +26,59 @@
         </transition>
         <br>
         <button type="button" @click="completeTodo(todo)" :aria-label="todo.done ? 'Undone' : 'Done'">
-          <i class="check circle outline icon"></i>
+          <i :class="done ? 'check circle outline icon' : 'check circle'"></i>
         </button>
-      </div>  
-      <div class="interaction" >
-        <transition name="fade" mode="out-in">
-          <div v-if="done" class="interaction-B" key="a">
-            <h6>Total Time:</h6>
-            {{total}}
-          </div>
-          <div v-else class="interaction-A" key="b">
-          </div>
-        </transition>
-      </div>
+      <!-- </div>  -->
     </div>
 
     <div id="appe">
 
       <div class="history">
-        <p>
-        How to make the green bordered area transition smoothly between different height in states A and B?
-        </p>
-        <div class="placeholder-content">
+        <button type="button"@click="deleteTodo(todo)" class="remove">
+          <i class="trash alternate icon"></i>
+        </button>
+        <div class="remove-after"></div>
+        <div style="display: block;">
+          <h4>{{todo.title}}</h4>
+          <h5>{{todo.text}}</h5>
         </div>
+        <div class="placeholder-content">
+          <hr>
+          <ul>
+            <li v-for="time in todo.times"> {{time}}</li>
+          </ul>
+        </div>
+        <div>
+          <transition name="slide-fade" mode="out-in">
+            <div v-if="!timing && !done" key="begin">
+              <!--  v-show="!timing && !done"-->
+              <button type="button" @click="startTime" > Begin </button>
+            </div>
+            <!--   v-show="timing && !done"-->
+            <div class="stopwatch" v-if="timing && !done" key="timer" >
+              <stopwatch v-on:calculated="calculated"></stopwatch>
+              <button type="button" @click="timing = !timing" v-show="timing"> Done For Now </button>
+            </div>
+          </transition>
+        </div>
+        <br>
+        <button type="button" @click="completeTodo(todo)" :aria-label="todo.done ? 'Undone' : 'Done'">
+          <i :class="done ? 'check circle outline icon' : 'check circle'"></i>
+        </button>
       </div>
 
-        <button @click="(show_A ? show_A = false : show_A = true);">
+        <button @click="(show_A ? show_A = false : show_A = true);" style="min-height: 20px;">
           Cycle states
         </button>
       <div class="interaction" v-bind:class="{ show_B: !show_A }">
+       
         
+        <br>
         <transition name="fade" >
           <div v-if="show_A" class="interaction-A" key="a"> A </div>
-          <div v-else class="interaction-B" key="b"> B </div>
+          <div v-else class="interaction-B" key="b"> 
+            <h3 v-show="!show_A">Completed</h3>
+          </div>
         </transition>
         
       </div>  
@@ -113,9 +133,9 @@ export default {
     transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 
   }
-  .remove {
+  /* .remove {
     float: right;
-  }
+  } */
   .remove-after {
     clear: both;
   }
@@ -147,11 +167,11 @@ export default {
   display: flex;
   flex: 999 1 auto;
   flex-direction: column-reverse;
-  overflow-y: auto;
+  /* overflow-y: auto; */
 }
 
 .placeholder-content {
-  min-height: 10px;
+  min-height: 500px;
 }
 
 .interaction-A {
@@ -180,23 +200,19 @@ export default {
     max-height: 325px;
     transition: max-height 0.15s ease-in;
 }
-/* // aa */
- 
 
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .10s;
+ }
 
+.fade-enter-active {
+  transition-delay: .25s;
+}
 
-  .fade-enter-active, .fade-leave-active {
-    transition-property: opacity;
-    transition-duration: .50s;
-  }
-
-  .fade-enter-active {
-    transition-delay: .25s;
-  }
-
-  .fade-enter, .fade-leave-active {
-    opacity: 0;
-  }
+.fade-enter, .fade-leave-active {
+  opacity: 0;
+ }
 
 
 </style>
