f9519d5d68151d1c8227100f66e9195c84892972
Windows Letgoner <jmw686@nyu.edu>
2018-03-19 17:30:49 -0500

    remove comments that were junk

--- productivity/src/components/Todo.vue
+++ productivity/src/components/Todo.vue
@@ -1,34 +1,5 @@
 <template lang="html">
   <div  class="card ui todo lefter">
-    <!-- <div>
-      <div class="history">
-        <button type="button"@click="deleteTodo(todo)" class="remove">
-          <i class="trash alternate icon"></i>
-        </button>
-        <div class="remove-after"></div>
-        <h4>{{todo.title}}</h4>
-        <h5>{{todo.text}}</h5>
-        <h3 v-show="done">Completed</h3>
-        <hr>
-        <ul>
-          <li v-for="time in todo.times"> {{time}}</li>
-        </ul>
-        <transition name="slide-fade" mode="out-in">
-          <div v-if="!timing && !done" key="begin">
-            <button type="button" @click="startTime" > Begin </button>
-          </div>
-          <div class="stopwatch" v-if="timing && !done" key="timer" >
-            <stopwatch v-on:calculated="calculated"></stopwatch>
-            <button type="button" @click="timing = !timing" v-show="timing"> Done For Now </button>
-          </div>
-        </transition>
-        <br>
-        <button type="button" @click="completeTodo(todo)" :aria-label="todo.done ? 'Undone' : 'Done'">
-          <i :class="done ? 'check circle outline icon' : 'check circle'"></i>
-        </button>
-      </div> 
-    </div> -->
-
     <div id="appe">
       <div class="history card ui todo lefter">
         <!-- <a href="https://stackoverflow.com/questions/45128744/vue-css-how-to-make-a-smooth-height-transition-between-two-alternating-elements" >AAAA</a> -->
@@ -47,10 +18,8 @@
         <div class="stopperwatch" :class="{bgc: done}">
           <transition name="slide-fade" mode="out-in">
             <div v-if="!timing && !done" key="begin" class="stopwatch buttons">
-              <!--  v-show="!timing && !done"-->
               <button class="ui button" type="button" @click="startTime"> Begin </button>
             </div>
-            <!--   v-show="timing && !done"-->
             <div class="stopwatch" v-if="timing && !done" key="timer">
               <stopwatch v-on:calculated="calculated"></stopwatch>
               <button class="ui button" type="button" @click="timing = !timing" v-show="timing"> Done For Now </button>
@@ -68,8 +37,7 @@
             <h3>Completed</h3>
             <p>{{total.sum}}</p>
           </div>
-        </transition>
-        
+        </transition>     
       </div>  
     </div>
   </div>
@@ -117,12 +85,7 @@ export default {
   .todo {
     margin: 0 auto;
     width: 400px;
-    /* overflow-y: auto;
-    display: flex;
-    flex-direction: column;
-    border: 10px solid lightgray; */
     transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-
   }
   .buttons {
     margin-top: 4em;
@@ -138,12 +101,10 @@ export default {
     margin: 0 auto;
   }
   .stopperwatch {
-    /* padding-bottom: 1em; */
     min-height: 200px;
     height: auto;
     max-height: 200px;
     overflow: hidden;
-    /* height: 120px; */
   }
   .stopwatch {
     height: 200px;
@@ -166,36 +127,27 @@ export default {
   }
   .slide-fade-enter-active {
     transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-    /* max-height: 5px; */
     padding-bottom: 1em;
   }
   .slide-fade-leave-active {
     transition: all .28s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-    /* max-height: 0px; */
-    /* padding-bottom: 1em; */
   }
   .slide-fade-enter, .slide-fade-leave-to
   /* .slide-fade-leave-active below version 2.1.8 */ {
-    /* transform: translateX(10px); */
     opacity: 0;
-    /*padding-bottom: 1em; */
   }
 
 
 #appe {
   display: flex;
-  /* height: 300px; */
   height:auto;
   flex-direction: column;
-  /* border: 10px solid lightgray */
 }
 
 .history {
-  /* border: 3px solid lightyellow; */
   display: flex;
   flex: 999 1 auto;
   flex-direction: column;
-  /* overflow-y: auto; */
 }
 
 .placeholder-content {
@@ -203,21 +155,16 @@ export default {
 }
 
 .interaction-A {
-  /* border: 3px solid lightblue; */
-  /* padding: 5px; */
-  /* margin: 5px; */
   min-height: 0px;
 }
 
 .interaction-B {
-  /* border: 3px solid lightblue; */
   padding: 5px;
   margin: 5px;
   min-height: 10px;
 }
 
 .interaction {
-  /* border: 10px solid lightgreen; */
   display: flex;
   flex: 1 0 auto;
   text-align: center;
